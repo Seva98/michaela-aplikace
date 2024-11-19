9b10385fc88cf0a0e5988b0e78589b71fddd8f73
Ondrej Sevcik <o.sevcik91@gmail.com>
2024-11-19 10:53:48 +0100

    Updated owner settings

--- components/layout/constants.ts
+++ components/layout/constants.ts
@@ -1,7 +1,7 @@
 import { Path } from './categories';
 
 export const navbarLinks = [
-  { href: Path.home, text: 'Osobní tréninky Plzeň' },
+  { href: Path.home, text: 'Aplikace' },
   { href: Path.about, text: 'O mně' },
   // { href: Path.blog, text: 'Blog' },
   // { href: Path.recipes, text: 'Recepty' },
--- components/layout/navbarLinks.tsx
+++ components/layout/navbarLinks.tsx
@@ -5,15 +5,21 @@ import { getOwnerSettings } from '@/db/ownerSettings/getOwnerSettings';
 const NavbarLinks = async () => {
   const ownerSettings = await getOwnerSettings();
 
-  return ownerSettings.menu
-    ? ownerSettings.menu.map(({ title, href }, index) => (
-        <Typography variant="h5" key={`navbar-link-${index}`}>
-          <Link className="uppercase" href={href}>
-            {title}
-          </Link>
-        </Typography>
-      ))
-    : null;
+  return ownerSettings.menu ? (
+    ownerSettings.menu.map(({ title, href }, index) => (
+      <Typography variant="h5" key={`navbar-link-${index}`}>
+        <Link className="uppercase" href={href}>
+          {title}
+        </Link>
+      </Typography>
+    ))
+  ) : (
+    <Typography variant="h5" key={`navbar-link-${1}`}>
+      <Link className="uppercase" href={'/'}>
+        Aplikace
+      </Link>
+    </Typography>
+  );
 };
 
 export default NavbarLinks;
