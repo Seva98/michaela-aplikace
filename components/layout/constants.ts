6a22871c6e420b44e998c235a153654ff3191b98
Ondrej Sevcik <o.sevcik91@gmail.com>
2024-11-19 11:08:38 +0100

    removed constants

--- components/layout/constants.ts
+++
@@ -1,9 +0,0 @@
-import { Path } from './categories';
-
-export const navbarLinks = [
-  { href: Path.home, text: 'Aplikace' },
-  { href: Path.about, text: 'O mně' },
-  // { href: Path.blog, text: 'Blog' },
-  // { href: Path.recipes, text: 'Recepty' },
-  // { href: Path.trainings, text: 'Osobní tréninky Plzeň' },
-];
